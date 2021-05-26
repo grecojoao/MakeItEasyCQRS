a0f642fd99150bdada7d2f0f74d5053f880a2af1
João Greco <junior.greco@hotmail.com>
2021-05-26 17:32:44 -0300

    Adicionados novos tipos de manipuladores

---
+++ samples/MakeItEasyCQRS.Examples.HowToUse/Handlers/CalculatorHandler.cs
@@ -0,0 +1,22 @@
+﻿using MakeItEasyCQRS.Commands;
+using MakeItEasyCQRS.Commands.Contracts;
+using MakeItEasyCQRS.Examples.HowToUse.Commands;
+using MakeItEasyCQRS.Handlers.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakeItEasyCQRS.Examples.HowToUse.Handlers
+{
+    class CalculatorHandler : IHandler<SumCommand>
+    {
+        public Task<ICommandResult> Handle(SumCommand command, CancellationToken cancellationToken = default)
+        {
+            command.Validate();
+            if (command.IsInvalid)
+                return Task.FromResult((ICommandResult)new CommandResult(false, command.NotificationsMessage(), command.Notifications));
+
+            var calculatedValue = Calculator.Sum((decimal)command.ValueOne, (decimal)command.ValueTwo);
+            return Task.FromResult((ICommandResult)new CommandResult(true, "Calculation done", calculatedValue));
+        }
+    }
+}
