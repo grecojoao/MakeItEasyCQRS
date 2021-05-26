8d6e1efbaafec39f3ef10f5c3c76a24b9efe74e1
João Greco <junior.greco@hotmail.com>
2021-05-26 17:26:09 -0300

    Implementando o projeto "How to use"

---
+++ src/MakeItEasyCQRS/Handlers/Contracts/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MakeItEasyCQRS.Commands.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakeItEasyCQRS.Handlers.Contracts
+{
+    public interface ICommandHandler<TCommand, TCommandResult>
+        where TCommand : ICommand
+        where TCommandResult : ICommandResult
+    {
+        Task<TCommandResult> Handle(TCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ src/MakeItEasyCQRS/Handlers/Contracts/IGenericHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakeItEasyCQRS.Handlers.Contracts
+{
+    public interface IGenericHandler<TRequest, TResult>
+    {
+        Task<TResult> Handle(TRequest request, CancellationToken cancellationToken);
+    }
+}
